f1079568abfb209738630b82ca2d9b534690f8b5
Ethan Lipnik <hi@ethanlipnik.com>
2021-06-11 15:35:37 -0400

    Improved favicons

--- Sources/Moji/Moji+RSS.swift
+++ Sources/Moji/Moji+RSS.swift
@@ -38,7 +38,7 @@ extension Moji {
             let channel = try? container.decode(RSS.self, forKey: .channel)
 
             self.title = channel?.title ?? (try? container.decode(String.self, forKey: .title))
-            self.link = channel?.link ?? (try? container.decode(URL.self, forKey: .link))
+            self.link = channel?.link ?? (try? container.decode(URL.self, forKey: .link)) ?? (try? container.decode(Link.self, forKey: .link).href)
             self.description = channel?.description ?? (try? container.decode(String.self, forKey: .description))
             self.language = channel?.language ?? (try? container.decode(String.self, forKey: .language))
             self.managingEditor = channel?.managingEditor ?? (try? container.decode(String.self, forKey: .managingEditor))
@@ -76,17 +76,26 @@ extension Moji {
         
         @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
         public func getFavicon() async throws -> URL {
-            guard let link = link?.absoluteString, !link.isEmpty else { throw URLError(.badURL) }
-            let strippedLink = link
-                .replacingOccurrences(of: "https://", with: "")
-                .replacingOccurrences(of: "http://", with: "")
-            guard let url = URL(string: "https://favicongrabber.com/api/grab/\(strippedLink)") else { throw URLError(.badURL) }
+            guard let link = link?.rootDomain else { throw URLError(.badURL) }
+            guard let url = URL(string: "https://favicongrabber.com/api/grab/\(link)") else { throw URLError(.badURL) }
             
             let urlRequest = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad)
             let (data, _) = try await URLSession.shared.data(for: urlRequest)
             
             guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any], let icons = json["icons"] as? [[String: String]], let iconStr = icons.first?["src"], let icon = URL(string: iconStr) else { throw URLError(.badServerResponse) }
             return icon
+        }
+    }
+}
+
+fileprivate extension URL {
+    var rootDomain: String? {
+        guard let hostName = self.host else { return nil }
+        let components = hostName.components(separatedBy: ".")
+        if components.count > 2 {
+            return components.suffix(2).joined(separator: ".")
+        } else {
+            return hostName
         }
     }
 }

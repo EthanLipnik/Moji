1cbfea0f43122b79724ec0505706f71387d7920c
Ethan Lipnik <hi@ethanlipnik.com>
2021-06-11 17:36:49 -0400

    Improved links and dates

--- Sources/Moji/Moji+Item.swift
+++ Sources/Moji/Moji+Item.swift
@@ -9,10 +9,11 @@ import Foundation
 
 extension Moji {
     public struct Item: Codable, Hashable, Equatable {
-        public var guid, title, description, pubDate, content: String?
+        public var guid, title, description, content: String?
+        public var pubDate: Date?
         public var link: URL?
         
-        public init(guid: String? = nil, title: String? = nil, description: String? = nil, pubDate: String? = nil, content: String? = nil, link: URL? = nil) {
+        public init(guid: String? = nil, title: String? = nil, description: String? = nil, pubDate: Date? = nil, content: String? = nil, link: URL? = nil) {
             self.guid = guid
             self.title = title
             self.description = description
@@ -35,11 +36,16 @@ extension Moji {
             let container = try decoder.container(keyedBy: CodingKeys.self)
 
             self.guid = try? container.decode(String.self, forKey: .guid)
-            self.link = try? container.decode(URL.self, forKey: .link)
+            self.link = (try? container.decode(URL.self, forKey: .link)) ?? (try? container.decode(Link.self, forKey: .link).href)
             self.title = try? container.decode(String.self, forKey: .title)
             self.description = try? container.decode(String.self, forKey: .description)
-            self.pubDate = try? container.decode(String.self, forKey: .pubDate)
             self.content = (try? container.decode(String.self, forKey: .content)) ?? (try? container.decode(String.self, forKey: .encoded))
+            
+            if let pubDateStr = try? container.decode(String.self, forKey: .pubDate) {
+                self.pubDate = RFC822DateFormatter().date(from: pubDateStr) ??
+                (RFC3339DateFormatter().date(from: pubDateStr) ??
+                ISO8601DateFormatter().date(from: pubDateStr))
+            }
         }
         
         public func encode(to encoder: Encoder) throws {
---
+++ Sources/Moji/Moji+Link.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ethan Lipnik on 6/11/21.
+//
+
+import Foundation
+import XMLCoder
+
+extension Moji {
+    struct Link: Codable, Hashable, DynamicNodeDecoding {
+        let href: URL
+        
+        enum CodingKeys: String, CodingKey {
+            case href
+        }
+        
+        static func nodeDecoding(for key: CodingKey) -> XMLDecoder.NodeDecoding {
+            switch key {
+            case CodingKeys.href:
+                return XMLDecoder.NodeDecoding.attribute
+            default:
+                return .element
+            }
+        }
+    }
+}
